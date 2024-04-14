7eb32c6b8c29f4e32daee631fac18580aa71c212
theDaveB <davebloisesquire@gmail.com>
2024-04-13 23:00:56 -0700

    change cname config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://davebloisesquire.github.io',
+    // site: 'https://davebloisesquire.github.io',
+    site: 'https://davedoesblog.com',
     base: 'dave-does-blog'
 });
